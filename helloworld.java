bce92f8447b29fb81437952434053b3fdcae2fec
goswamirajesh <97104782+goswamirajesh@users.noreply.github.com>
2022-06-19 19:52:38 +0300

    webhook trigge

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public class helloworld {
    public static void main(String[] args) {
     System.out.println("Hello World 19th june 9:58 PM IST");
-      System.out.println("Hello World 19th june 7:37 PM EST");
+    System.out.println("Hello World 19th june 7:37 PM EST");
+     System.out.println("Triggered by webhook");
     }
 }
